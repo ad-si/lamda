210dc5e45ea6754ab3d4711ead9ac6dd10443936
adius <mail@adriansieber.com>
2014-04-19 22:14:33 +0200

    Added email-address favicons, added mledoze/countries, added & integrated country-flags

--- util.js
+++ util.js
@@ -1,33 +1,51 @@
-module.exports.writeKeys = function (keysObject, data) {
+var countries = require('./public/js/countries.json'),
+	util = {}
+
+
+function getFromArray(array, key, value) {
+
+	var soughtElement = null
+
+	array.some(function (element) {
+		if (element[key] == value) {
+			soughtElement = element
+			return true
+		}
+	})
+
+	return soughtElement
+}
+
+function capitalize(string) {
+	return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase()
+}
+
+util.writeKeys = function (keysObject, data) {
 
 	for (var key in data)
 		if (data.hasOwnProperty(key))
 			keysObject[key] = true
 }
 
-module.exports.formatData = function (data) {
+util.formatData = function (data) {
 
 	data.gender = data.gender || ''
 
 
-	function formatAddress(addr){
+	function formatAddress(addr) {
 
-		var addrArray = []
+		if (addr.country) {
 
-
-		if (addr.country) addrArray.push(addr.country)
-
-		if (addr.zip && addr.city)
-			addrArray.push(addr.zip + ' ' + addr.city)
-		else {
-			if (addr.zip) addrArray.push(addr.zip)
-			if (addr.city) addrArray.push(addr.city)
+			if (addr.country.length === 2)
+				addr.countryCode = addr.country
+			else
+				addr.countryCode =
+					getFromArray(countries, 'name', capitalize(addr.country))
+						.cca2
+						.toLowerCase()
 		}
 
-		if (addr.street) addrArray.push(addr.street)
-		if (addr.addition) addrArray.push(addr.addition)
-
-		return addrArray.join(', ')
+		return addr
 	}
 
 
@@ -37,3 +55,8 @@ module.exports.formatData = function (data) {
 
 	return data
 }
+
+util.getFromArray = getFromArray
+util.capitalize = capitalize
+
+module.exports = util
