e76cff505dbc280d8bcd07fc82bd5625db7e7e59
adius <mail@adriansieber.com>
2014-04-21 00:06:09 +0200

    Adapted Contacts, Files & Things apps to new format

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ var fs = require('fs'),
 	express = require('express'),
 	stylus = require('stylus'),
 	path = require('path'),
+	files = require('./routes/files'),
 	app = express()
 
 
 app.set('views', __dirname + '/views')
 
-
-app.get('/', function (request, response) {
-
-	response.render('music')
-})
+app.get(/(.*)/, files)
 
 module.exports = app
---
+++ routes/files.js
@@ -0,0 +1,76 @@
+var fs = require('fs'),
+
+	pathToJson = require('../../../api/pathToJson'),
+	fsToJson = require('../../../api/fsToJson')
+//files = require('../api/files'),
+
+
+module.exports = function (req, res) {
+
+	var pathParam = req.params[0] || '',
+		columns = [],
+		depth = 0,
+		path = ''
+
+
+	function buildColumns(element, children) {
+
+		if (children) {
+
+			if (!columns[depth])
+				columns[depth] = {
+					active: '',
+					path: path,
+					entries: []
+				}
+
+			children.forEach(function (child) {
+
+				if (typeof child === 'object') {
+
+					columns[depth].path = path
+					path = path + '/' + child.name
+
+					columns[depth].active = child.name
+					columns[depth].entries.push(child.name)
+					depth++
+					buildColumns(child, child.children)
+					depth--
+				}
+				else
+					columns[depth].entries.push(child)
+			})
+		}
+		else {
+			columns[depth] = {
+				file: element
+			}
+
+			// TODO: Map file types to functions
+
+			if (columns[depth].file.type === 'text')
+				columns[depth].file.content = fs
+					.readFileSync(columns[depth].file.path, 'utf8')
+
+			else if (columns[depth].file.type === 'yaml')
+				columns[depth].file.content = fs
+					.readFileSync(columns[depth].file.path, 'utf8')
+		}
+
+
+		return columns
+	}
+
+	//console.log(baseURL, pathParam)
+	//console.log(JSON.stringify(fsToJson(baseURL), null, 2))
+	//console.log(JSON.stringify(pathToJson(baseURL, pathParam), null, 2))
+	//console.log(JSON.stringify(buildColumns(pathToJson(baseURL, pathParam).children), null, 2))
+
+	res.render('index', {
+		page: 'files',
+		columns: buildColumns(
+			pathToJson(global.baseURL, pathParam),
+			pathToJson(global.baseURL, pathParam).children
+		)
+	})
+}
