f4c5f755bc05d89af389890adba08e62e19ca5ce
adius <mail@adriansieber.com>
2014-04-20 23:03:45 +0200

    Adapted Events app to new format

---
+++ index.js
@@ -0,0 +1,16 @@
+var fs = require('fs'),
+	express = require('express'),
+	stylus = require('stylus'),
+	path = require('path'),
+	app = express()
+
+
+app.set('views', __dirname + '/views')
+
+
+app.get('/', function (request, response) {
+
+	response.render('music')
+})
+
+module.exports = app
