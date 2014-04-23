132bc971641abbd643e6bd150c6a8ed0c14e440e
adius <mail@adriansieber.com>
2014-04-23 11:04:50 +0200

    Started profile, added functionality to add tasks in Tasks app

--- index.js
+++ index.js
@@ -12,4 +12,10 @@ app.get('/', function (request, response) {
 	response.render('index')
 })
 
+//app.get('/music', apps.Music.module.index)
+//app.get('/music/songs', apps.Music.module.songs)
+//app.get('/music/artists', apps.Music.module.artists)
+//app.get('/music/:artist', apps.Music.module.artist)
+//app.get('/music/:artist/:song', apps.Music.module.song)
+
 module.exports = app
