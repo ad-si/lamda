1b8e03a46fdb98fa1803385431b7af10c419a6f1
adius <mail@adriansieber.com>
2014-05-13 21:59:17 +0200

    Load and display rss-feed

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,67 @@
 var fs = require('fs'),
-	path = require('path'),
-	yaml = require('js-yaml')
+    path = require('path'),
+    yaml = require('js-yaml'),
+    Feedparser = require('feedparser'),
+    feedparser = new Feedparser,
+    Crawler = require('crawler').Crawler,
+    crawler = new Crawler({
+	    maxConnections: 10,
+	    callback: function (error, result, $) {
+
+		    $("#content a").each(function (index, a) {
+			    //c.queue(a.href)
+			    console.log(a.href)
+		    })
+	    }
+    })
 
 
 module.exports = function (req, res) {
 
-	var data = [
-		{
-			title: 'Datum 1'
-		},
-		{
-			title: 'Datum 2'
-		},
-		{
-			title: 'Datum 3'
-		}
-	]
+	var FeedParser = require('feedparser'),
+	    request = require('request'),
+	    url = global.config.News.url,
+	    feedReq = request(url),
+	    feedparser = new FeedParser(),
+	    articles = []
 
-	res.render('index', {
-		data: data
+	//crawler.queue(url)
+
+
+	feedReq.on('error', function (error) {
+		throw new Error(error)
+	})
+
+	feedReq.on('response', function (res) {
+
+		var stream = this
+
+		if (res.statusCode != 200)
+			return this.emit('error', new Error('Bad status code'))
+
+		stream.pipe(feedparser)
+	})
+
+
+	feedparser.on('error', function (error) {
+		throw new Error(error)
+	})
+
+	feedparser.on('readable', function () {
+		// This is where the action is!
+		var stream = this,
+		    meta = this.meta,
+		    item
+
+		while (item = stream.read()) {
+			articles.push(item)
+		}
+	})
+
+	feedparser.on('end', function () {
+		res.render('index', {
+			page: 'news',
+			articles: articles
+		})
 	})
 }
