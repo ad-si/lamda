3bb81d54dd090b0d78e99324ef4b0f71e5c055fa
adius <mail@adriansieber.com>
2014-11-16 16:49:17 +0100

    Initial commit with boilerplate code

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var fs = require('fs'),
+	path = require('path'),
+	yaml = require('js-yaml')
+
+
+module.exports = function (req, res) {
+
+	var data = [
+		{
+			title: 'Datum 1'
+		},
+		{
+			title: 'Datum 2'
+		},
+		{
+			title: 'Datum 3'
+		}
+	]
+
+	res.render('index', {
+		page: 'boilerplate',
+		data: data
+	})
+}
