ddaa4762d90559d8bb4d40d5c86206c010d139f9
adius <mail@adriansieber.com>
2014-11-16 16:19:21 +0100

    List projects with number of commits

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,98 @@
 var fs = require('fs'),
 	path = require('path'),
-	yaml = require('js-yaml')
+	yaml = require('js-yaml'),
+	nodegit = require('nodegit'),
 
+	projectsDir = path.join(global.baseURL, 'projects')
+
+
+function getNumberOfCommits (repoDir, callback) {
+
+	nodegit.Repo.open(repoDir, function (error, repo) {
+
+		if (error) {
+			callback(error)
+			return
+		}
+
+		repo.getMaster(function (error, branch) {
+
+			var history,
+				numberOfCommits = 0
+
+			if (error) {
+				callback(error)
+				return
+			}
+
+			history = branch.history()
+
+			history.on('commit', function () {
+				numberOfCommits++
+			})
+
+			history.on('end', function () {
+				callback(null, numberOfCommits)
+			})
+
+			history.start()
+		})
+	})
+}
 
 module.exports = function (req, res) {
 
-	var data = [
-		{
-			title: 'Datum 1'
-		},
-		{
-			title: 'Datum 2'
-		},
-		{
-			title: 'Datum 3'
-		}
-	]
+	var projectDirs = fs.readdirSync(projectsDir),
+		projectsCounter = projectDirs.length,
+		projects = []
 
-	res.render('index', {
-		page: 'boilerplate',
-		data: data
+
+	function render () {
+
+		if (projects.length === projectsCounter)
+			res.render('index', {
+				page: 'Projects',
+				projects: projects.reverse(),
+				numberOfCommits: projects
+					.map(function (project) {
+						return project.numberOfCommits
+					})
+					.reduce(function (a, b) {
+						return a + b
+					})
+			})
+	}
+
+	projectDirs.forEach(function (projectDir) {
+
+		var absoluteProjectPath = path.join(projectsDir, projectDir)
+
+
+		fs.lstat(absoluteProjectPath, function (error, stats) {
+
+			if (error) throw new Error(error)
+
+			if (!stats.isDirectory()) {
+				projectsCounter--
+				render()
+			}
+			else
+				getNumberOfCommits(
+					absoluteProjectPath,
+					function (error, numberOfCommits) {
+
+						if (error)
+							numberOfCommits = null
+
+						projects.push({
+							id: projectDir,
+							name: projectDir,
+							numberOfCommits: numberOfCommits
+						})
+
+						render()
+					}
+				)
+		})
 	})
 }
