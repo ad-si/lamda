1aa568b5fdaf9db4d5a3c74edbd8a4e21f83d282
adius <mail@adriansieber.com>
2014-12-05 20:06:58 +0100

    Implemented events overview with promises

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,61 @@
 var fs = require('fs'),
 	path = require('path'),
-	yaml = require('js-yaml')
+	yaml = require('js-yaml'),
+
+	photosDir = path.join(global.baseURL, 'photos')
 
 
-module.exports = function (req, res) {
+require('es6-promise').polyfill()
 
-	var data = [
-		{
-			title: 'Datum 1'
-		},
-		{
-			title: 'Datum 2'
-		},
-		{
-			title: 'Datum 3'
-		}
-	]
 
-	res.render('index', {
-		page: 'boilerplate',
-		data: data
+function getFiles (path) {
+	return new Promise(function (fulfill, reject) {
+		fs.readdir(path, function (error, files) {
+			if (error)
+				reject(error)
+			else
+				fulfill(files)
+		})
 	})
+}
+
+function filterYears (list) {
+	return list.filter(function (file) {
+		return /^[0-9]{4}$/.test(file)
+	})
+}
+
+function getEventsForYear (year) {
+	return getFiles(path.join(photosDir, year))
+		.then(function (files) {
+			return files.filter(function (file) {
+				return /[0-9]{4}-[01][0-9]-[0-3][0-9]_.*/.test(file)
+			})
+		})
+		.then(function (events) {
+			return {
+				year: Number(year),
+				events: events
+			}
+		})
+}
+
+
+module.exports = function (req, res, next) {
+
+	getFiles(photosDir)
+		.then(filterYears)
+		.then(function (years) {
+			return Promise.all(years.map(getEventsForYear))
+		})
+		.then(function (yearObjects) {
+			res.render('index', {
+				page: 'Photos',
+				years: yearObjects || []
+			})
+		})
+		.catch(function (error) {
+			console.error(error)
+			next()
+		})
 }
