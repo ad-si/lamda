0024a7959030366e7368461cf99f339995b0e2df
adius <mail@adriansieber.com>
2014-12-21 11:03:18 +0100

    Added filter icons, added type-filter functionality

--- public/index.js
+++ public/index.js
@@ -2,15 +2,56 @@
 
 !function () {
 
-	$('img')
-		.one('load', function (event) {
-			$(event.target)
-				.closest('a')
-				.css('background-image', 'none')
-		})
-		.each(function () {
-			if (this.complete)
-				$(this).load()
-		})
+	function hideSpinner (event) {
+		$(event.target)
+			.closest('a')
+			.css('background-image', 'none')
+	}
 
+	function checkCompletion () {
+
+		if (this.complete)
+			$(this).load()
+	}
+
+	function hideSpinnerOnLoad (image) {
+
+		image.addEventListener('load', function () {
+			image
+				.parentElement
+				.style['background-image'] = 'none'
+		})
+	}
+
+	$('img')
+		.one('load', hideSpinner)
+		.each(checkCompletion)
+
+	$('.filter').click(function (event) {
+
+		$('#thingsContainer').empty()
+
+		var filterType = event.delegateTarget.id
+
+		things
+			.filter(function (thing) {
+				return (filterType === 'all') || (thing.type === filterType)
+			})
+			.forEach(function (thing) {
+
+				shaven(
+					[$('#thingsContainer')[0],
+						['div',
+							['a', {href: thing.url},
+								['img',
+									{src: thing.image},
+									hideSpinnerOnLoad
+								]
+							]
+						]
+					]
+				)
+			})
+
+	})
 }()
