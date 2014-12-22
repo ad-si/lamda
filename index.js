38b913faf6cfb36db9faf48f7bedaadfbb8a6104
adius <mail@adriansieber.com>
2014-12-22 22:40:38 +0100

    Added lilyware middleware

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 var fs = require('fs'),
+	path = require('path'),
+
 	express = require('express'),
 	stylus = require('stylus'),
-	path = require('path'),
+	lilyware = require('lilyware'),
+
 	sheetmusic = require('./routes/sheetmusic'),
 	playlists = require('./routes/playlists'),
-	app = express()
+
+	app = express(),
+	basePath = path.join(global.baseURL, 'sheetmusic', 'songs')
 
 
-app.use(express.static(path.join(global.baseURL, 'sheetmusic')))
+app.use(lilyware(basePath))
+app.use(express.static(basePath))
 
 app.set('views', __dirname + '/views')
 
 app.get('/', sheetmusic.songs)
+
 app.get('/playlists', playlists.all)
 app.get('/playlist/:id', playlists.one)
-app.get('/:name', sheetmusic.song)
 
+app.get('/:name', sheetmusic.song)
 app.get('/raw/:name', sheetmusic.raw)
 
 module.exports = app
