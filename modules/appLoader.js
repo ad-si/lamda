e6ac56bc277aa11da4b7c57782fe5703bcfcfcaf
Adrian Sieber <mail@adriansieber.com>
2015-06-11 08:37:56 +0200

    Add option to load bundled node-modules apps

--- modules/appLoader.js
+++ modules/appLoader.js
@@ -3,65 +3,96 @@ var fs = require('fs'),
 	yaml = require('js-yaml')
 
 
-module.exports = function (rootApp, locals) {
 
-	var appNames = fs.readdirSync('./apps'),
-		apps = {}
-
-	appNames = appNames.filter(function (name) {
-
-		var appPath = path.join('apps', name)
-
-		return fs.lstatSync(appPath).isDirectory() && name !== 'boilerplate'
-	})
-
-
-	appNames.forEach(function (appName) {
-
-		var appPath = path.join(global.projectURL, 'apps', appName),
-			appModule,
-			packageContent
-
-		try {
-			appModule = require(appPath)
-		}
-		catch (error) {
-			console.error(
-				'The error', error,
-				'occured While loading the app:', appName
+function getPackageContent (appPath) {
+	if (fs.existsSync(path.join(appPath, 'package.yaml'))) {
+		return yaml.safeLoad(
+			fs.readFileSync(
+				path.join(appPath, 'package.yaml'),
+				'utf-8'
 			)
-			return
-		}
-
-		if (fs.existsSync(appPath + '/package.yaml')) {
-			packageContent = yaml.safeLoad(
-				fs.readFileSync(path.join(appPath, 'package.yaml'), 'utf-8')
+		)
+	}
+	else if (fs.existsSync(path.join(appPath, 'package.json'))) {
+		return JSON.parse(
+			fs.readFileSync(
+				path.join(appPath, 'package.json'),
+				'utf-8'
 			)
-		}
-		else if (fs.existsSync(path.join(appPath, 'package.json'))) {
-			packageContent = JSON.parse(
-				fs.readFileSync(path.join(appPath, 'package.json'), 'utf-8')
-			)
-		}
-		else
-			throw new Error('Package file is missing!')
+		)
+	}
+	else
+		throw new Error('Package file is missing!')
+}
 
 
-		apps[appName] = packageContent
+function addAppToAppMap (appMap, appPath, rootApp, locals) {
 
-		if (!apps[appName].hasOwnProperty('lamda'))
-			apps[appName].lamda = {}
+	var appPath = path.join(global.projectURL, appPath),
+		appName = path.basename(appPath),
+		appModule
 
+	try {
+		appModule = require(appPath)
 
-		apps[appName].lamda.module = appModule
-		apps[appName].lamda.path = appPath
+		appMap[appName] = getPackageContent(appPath)
 
+		if (!appMap[appName].hasOwnProperty('lamda'))
+			appMap[appName].lamda = {}
+
+		appMap[appName].lamda.module = appModule
+		appMap[appName].lamda.path = appPath
 
 		appModule.locals = locals
 		appModule.locals.page = appName
 
 		rootApp.use('/' + appName, appModule)
-	})
+	}
+	catch (error) {
+		console.error('Following error occured while loading the app:', appName)
+		console.error(error.stack)
+	}
 
-	return apps
+	return appMap
+}
+
+
+module.exports = function (rootApp, locals) {
+
+	var appDirectories,
+		appNames
+
+	if (!fs.existsSync('apps'))
+		appDirectories = [
+			'books',
+			'contacts',
+			'events',
+			'files',
+			'movies',
+			'music',
+			'news',
+			'sheetmusic',
+			'tasks',
+			'things',
+			'photos',
+			'projects'
+		]
+		.map(function (name) {
+			return path.join('node_modules', name)
+		})
+	else
+		appDirectories = fs
+			.readdirSync('./apps')
+			.map(function () {
+				return path.join('apps', name)
+			})
+
+	return appDirectories
+		.filter(function (appPath) {
+			return fs.statSync(appPath).isDirectory() &&
+				path.basename(appPath) !== 'boilerplate'
+		})
+		.reduce(function (map, appPath) {
+			return addAppToAppMap(map, appPath, rootApp, locals)
+		}, {})
 }
