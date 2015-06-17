276ba5087aea63e82e8cc01d0681ddc6c59fcd28
Adrian Sieber <mail@adriansieber.com>
2015-06-17 22:51:31 +0200

    Use vanilla javascript instead of jquery, remove dead code

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,23 @@
 !function (window, document) {
 
-	Mousetrap.bind('/', function(){
-		$('#console').toggle()
-	})
-
-	$('select#theme').on('change', function () {
-		$('#themeLink').attr('href', '/styles/themes/' + this.value +'.css')
-	})
-
-	/*files.addEvenetListener('click', function(event){
-		event.preventDefault()
-
-		var depth = $(this)
-			.parentsUntil('#columns')
-			.data('depth')
-
-		console.log(depth)
-
-	})
-
-	$(document)
-		.on('click', '#columns a', function (event) {
-			event.preventDefault()
-
-			$.getJSON('/api/files/' + this.innerHTML, function (data) {
-				console.log(data)
-			})
+	if (typeof Mousetrap !== 'undefined')
+		Mousetrap.bind('/', function(){
+			document
+				.getElementById('console')
+				.toggle()
 		})
-	*/
+
+	var themeSelector = document.getElementById('theme')
+
+	if (themeSelector)
+		themeSelector
+			.addEventListener('change', function () {
+				document
+					.getElementById('themeLink')
+					.setAttribute(
+						'href',
+						'/styles/themes/' + this.value +'.css'
+					)
+			})
 
 }(window, document)
