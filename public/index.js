3962ade7d1b60b12a3d4aad077fa5e47edee37cf
Adrian Sieber <mail@adriansieber.com>
2015-06-20 10:42:58 +0200

    Only activate book viewer for epubs, replace jquery with vanilla js

--- public/index.js
+++ public/index.js
@@ -1,28 +1,38 @@
-!function () {
+!function (document, window) {
 
-	var book = ePub({
-		restore: true,
-		styles: {
-			color: 'rgb(200,200,200)'
-		}
-	})
+	if (/\.epub$/.test(bookUrl)) {
 
-	book.open(bookUrl)
-
-	book.renderTo('bookContent')
-
-	$('#previous').click(function () {
-		book.prevPage()
-	})
-
-	$('#next').click(function () {
-		book.nextPage()
-	})
-
-	book
-		.getMetadata()
-		.then(function (meta) {
-			console.log(meta)
+		var book = ePub({
+			restore: true,
+			styles: {
+				color: 'rgb(200,200,200)'
+			}
 		})
 
-}()
+		book.open(bookUrl)
+
+		book.renderTo('bookContent')
+
+		document
+			.getElementById('previous')
+			.addEventListener('click', function () {
+				book.prevPage()
+			})
+
+		document
+			.getElementById('next')
+			.addEventListener('click', function () {
+				book.nextPage()
+			})
+
+		book
+			.getMetadata()
+			.then(function (meta) {
+				console.log(meta)
+			})
+	}
+	else {
+
+	}
+
+}(document, window)
