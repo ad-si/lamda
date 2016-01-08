0e48545468659737d65768f786580750555b2a45
Adrian Sieber <mail@adriansieber.com>
2016-01-08 23:51:28 +0100

    Fix image-resizer-middleware require path

--- routes/pieces.js
+++ routes/pieces.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const path = require('path')
 
 const yaml = require('js-yaml')
-const imageResizer = require('image-resizer')
+const imageResizer = require('image-resizer-middleware')
 const isImage = require('is-image')
 const userHome = require('user-home')
 
