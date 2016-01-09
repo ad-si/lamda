1bdb7e451bc40b849b220c402e12ff5e451c5b36
Adrian Sieber <mail@adriansieber.com>
2016-01-09 16:58:48 +0100

    Correctly load styles files and other assets

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const userHome = require('user-home')
 global.basePath = global.basePath || userHome
 global.projectPath = global.projectPath || userHome
 global.baseURL = '/things'
+const thingsPath = path.join(global.basePath, 'things')
+const publicPath = path.join(__dirname, 'public')
+const viewsPath =  path.join(__dirname, 'views')
+const stylesPath = path.join(publicPath, 'styles')
+const modulesPath = path.join(__dirname, 'node_modules')
 
 const things = require('./routes/things')
 const thing = require('./routes/thing')
@@ -24,7 +29,7 @@ const isDevMode = app.get('env') === 'development'
 if (!isMounted) {
 	global.baseURL = ''
 
-	const faviconPath = path.join(__dirname, 'public/images/favicon.ico')
+	const faviconPath = path.join(publicPath, 'images/favicon.ico')
 	app.use(serveFavicon(faviconPath))
 
 	app.locals.styles = [{
@@ -32,17 +37,25 @@ if (!isMounted) {
 		id: 'themeLink',
 	}]
 	app.use(stylus.middleware({
-		src: path.join(__dirname,'node_modules/lamda-styles/themes'),
-		dest: path.join(__dirname, 'public/styles'),
+		src: path.join(modulesPath, 'lamda-styles/themes'),
+		dest: stylesPath,
 		debug: isDevMode,
 		compress: !isDevMode,
 	}))
 }
 
-// app.use(imageResizer.middleware('things'))
-app.use(express.static(path.join(global.basePath, 'things')))
+app.use(stylus.middleware({
+	src: stylesPath,
+	debug: isDevMode,
+	compress: !isDevMode,
+}))
+app.use(express.static(publicPath))
 
-app.set('views', path.join(__dirname, 'views'))
+// app.use(imageResizer.middleware('things'))
+app.use(express.static(thingsPath))
+app.use('/node_modules', express.static(modulesPath))
+
+app.set('views',viewsPath)
 
 app.get('/', things)
 app.get('/:id', thing)
