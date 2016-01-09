cede980896fabc46d9395d3adc8a6804e80b9d7d
Adrian Sieber <mail@adriansieber.com>
2016-01-09 15:25:56 +0100

    Add possibility to run app in standalone mode

--- server.js
+++ server.js
@@ -8,10 +8,29 @@ var fs = require('fs'),
 	thing = require('./routes/thing'),
 
 	app = express()
+const isMounted = Boolean(module.parent)
+const isDevMode = app.get('env') === 'development'
 
 
 app.use(imageResizer.middleware('things'))
 app.use(express.static(path.join(global.baseURL, 'things')))
+if (!isMounted) {
+	global.baseURL = ''
+
+	const faviconPath = path.join(__dirname, 'public/images/favicon.ico')
+	app.use(serveFavicon(faviconPath))
+
+	app.locals.styles = [{
+		path: '/styles/dark.css',
+		id: 'themeLink',
+	}]
+	app.use(stylus.middleware({
+		src: path.join(__dirname,'node_modules/lamda-styles/themes'),
+		dest: path.join(__dirname, 'public/styles'),
+		debug: isDevMode,
+		compress: !isDevMode,
+	}))
+}
 
 app.set('views', path.join(__dirname, 'views'))
 
@@ -19,3 +38,10 @@ app.get('/', things)
 app.get('/:id', thing)
 
 module.exports = app
+
+if (!isMounted) {
+	const port = 3000
+	app.set('view engine', 'jade')
+	app.listen(port)
+	console.log('App listens on http://localhost:' + port)
+}
