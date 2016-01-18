0caf68961c58d0470ed4714767201ee825f52d9b
Adrian Sieber <mail@adriansieber.com>
2016-01-18 22:47:41 +0100

    Fix standalone mode, add favicon

--- server.js
+++ server.js
@@ -5,10 +5,33 @@ let path = require('path')
 
 let express = require('express')
 let stylus = require('stylus')
+const serveFavicon = require('serve-favicon')
 
 let events = require('./routes/events')
 
 let app = express()
+const isDevMode = (app.get('env') === 'development')
+
+
+if (!module.parent) {
+	app.use(serveFavicon(path.join(__dirname, 'public/images/favicon.ico')))
+
+	app.locals.styles = [{
+		path: '/styles/dark.css',
+		id: 'themeLink',
+	}]
+	app.use(stylus.middleware({
+		src: path.join(__dirname, 'linked_modules/lamda-styles/themes'),
+		dest: path.join(__dirname, 'public/styles'),
+		compress: !isDevMode,
+	}))
+}
+
+app.use(stylus.middleware({
+	src: path.join(__dirname, 'public/styles'),
+	compress: !isDevMode,
+}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.set('views', __dirname + '/views')
@@ -18,9 +41,8 @@ app.get('/', events)
 module.exports = app
 
 if (!module.parent) {
+	const port = 3000
 	app.set('view engine', 'jade')
-
-	let port = 3000
-	app.listen(3000)
+	app.listen(port)
 	console.log('App listens on http://localhost:' + port)
 }
