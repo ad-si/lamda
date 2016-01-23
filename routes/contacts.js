de193417bf1e21ad2d1913c81aa8642a1b6bdd12
Adrian Sieber <mail@adriansieber.com>
2016-01-23 11:28:11 +0100

    Load contacts with fs-promise

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fsp = require('fs-promise')
 const path = require('path')
 
 const yaml = require('js-yaml')
@@ -10,59 +10,53 @@ global.basePath = global.basePath || userHome
 const contactsPath = path.join(global.basePath, 'contacts')
 
 
-module.exports = function (req, res) {
+module.exports = (request, response) => {
 
-	var contacts = [],
-		files = fs.readdirSync(contactsPath),
-		keysCollection = []
+	const keys = new Set()
+	const encoding = 'utf-8'
+	const yamlRegex = /\.yaml$/i
 
+	fsp
+		.readdir(contactsPath)
+		.then(fileNames => fileNames
+			.filter(fileName => yamlRegex.test(fileName))
+			.map(fileName => fsp.readFile(
+				path.join(contactsPath, fileName),
+				encoding
+			))
+		)
+		.then(contactFilePromises => Promise.all(contactFilePromises))
+		.then(contactFiles => contactFiles
+			.map(contactFile => yaml.safeLoad(contactFile))
+			.map(contactData => {
+				Object
+					.keys(contactData)
+					.forEach(key => keys.add(key))
 
-	files
-		.filter(function(fileName){
-			return /\.yaml$/i.test(fileName)
-		})
-		.forEach(function (file, index, contactFiles) {
-			fs.readFile(
-				path.join(contactsPath, file),
-				{encoding: 'utf-8'},
-				function (error, fileContent) {
-
-					if (error)
-						throw error
-
-					var jsonContactData = yaml.safeLoad(fileContent)
-
-					Object
-					.keys(jsonContactData)
-					.forEach(function (key) {
-						keysCollection[key] = true
-					})
-
-					contacts.push(formatContact(jsonContactData))
-
-
-					if (contacts.length === contactFiles.length) {
-
-						res.render('contacts', {
-							page: 'contacts',
-							contacts: contacts.sort(function(previous, current){
-								return previous.name > current.name
-							}),
-							availableKeys: Object.keys(keysCollection),
-							sortedKeys: [
-								'name',
-								'nickname',
-								'gender',
-								'birthday',
-								'email',
-								'mobile',
-								'website',
-								'facebook',
-								'address'
-							]
-						})
-					}
-
+				return formatContact(contactData)
+			})
+			.sort((previous, current) =>
+				previous.name.localeCompare(current.name)
+			)
+		)
+		.then(sortedContacts => {
+			response.render(
+				'contacts',
+				{
+					page: 'contacts',
+					contacts: sortedContacts,
+					availableKeys: Array.from(keys),
+					sortedKeys: [
+						'name',
+						'nickname',
+						'gender',
+						'birthday',
+						'email',
+						'mobile',
+						'website',
+						'facebook',
+						'address'
+					]
 				}
 			)
 		})
