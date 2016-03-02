e08342f650de397a6d97873e3a3de7917cae0b42
Adrian Sieber <mail@adriansieber.com>
2016-03-02 17:12:11 +0100

    Add "select all" and "select none" buttons

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,19 +3,68 @@
 const Ybdb = require('ybdb')
 const db = new Ybdb()
 
+// Contacts is set in script tag in HTML
 db.object.contacts = contacts
 
+const selectNoneButton = document.getElementById('selectNone')
+const selectAllButton = document.getElementById('selectAll')
+const tableBody = document.querySelector('tbody')
+let numberOfSelectedContacts = 0
 
-// Selection of contacts
-Array.from(document.querySelectorAll('tr a'))
+function setSelectButtonsVisibility () {
+	if (numberOfSelectedContacts === 0) {
+		selectNoneButton.classList.add('hidden')
+		selectAllButton.classList.remove('hidden')
+	}
+	else if (numberOfSelectedContacts === contacts.length) {
+		selectAllButton.classList.add('hidden')
+		selectNoneButton.classList.remove('hidden')
+	}
+	else {
+		selectNoneButton.classList.remove('hidden')
+		selectAllButton.classList.remove('hidden')
+	}
+}
+
+
+// Disable propagation of clicks on links
+Array.from(tableBody.querySelectorAll('tr a'))
 	.forEach(link => link.addEventListener('click', (event) => {
 		event.stopPropagation()
 	}))
 
-document
-	.querySelector('tbody')
-	.addEventListener('click', (event) => {
-		event.path
-			.find(element => element.tagName === 'TR')
-			.classList.toggle('selected')
-	})
+// Select/deselect contacts on click
+tableBody.addEventListener('click', (event) => {
+	const clickedRow = event.path.find(element => element.tagName === 'TR')
+
+	if (clickedRow) {
+		clickedRow.classList.toggle('selected')
+
+		numberOfSelectedContacts +=
+			clickedRow.classList.contains('selected') ? +1 : -1
+		setSelectButtonsVisibility()
+	}
+})
+
+
+// Select all
+selectAllButton.addEventListener('click', () => {
+	const unselectedContacts = tableBody.querySelectorAll('tr:not(.selected)')
+	Array
+		.from(unselectedContacts)
+		.forEach(row => row.classList.add('selected'))
+
+	numberOfSelectedContacts += unselectedContacts.length
+	console.log(numberOfSelectedContacts, contacts.length)
+
+	setSelectButtonsVisibility()
+})
+
+// Select none
+selectNoneButton.addEventListener('click', () => {
+	Array
+		.from(tableBody.querySelectorAll('tr.selected'))
+		.forEach(row => row.classList.remove('selected'))
+	numberOfSelectedContacts = 0
+	setSelectButtonsVisibility()
+})
