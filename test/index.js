4b565d42b9bd389c829ee02e04e07b5c45003789
Adrian Sieber <mail@adriansieber.com>
2016-10-28 10:44:22 +0000

    Add command line interface to create events

--- test/index.js
+++ test/index.js
@@ -1,18 +1,7 @@
-'use strict'
+const path = require('path')
+const runTest = require('ava')
+const loadEvents = require('../modules/loadEvents')
 
-let path = require('path')
-let runTest = require('ava')
-let moment = require('moment')
-
-let dummyApi = require('./_dummyApi')
-let processEvents = require('../modules/processEvents')
-let loadEvents = require('../modules/loadEvents')
-let events = []
-
-
-runTest.only('Processing of events', (test) => {
-	return loadEvents(path.join(__dirname, 'events'))
-		.then(files => {
-			processEvents(files)
-		})
+runTest.only('Processing of events', () => {
+  return loadEvents(path.join(__dirname, 'events'))
 })
