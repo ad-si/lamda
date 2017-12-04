f5e4a88b82b6567868d84c96c7adb52cd61af77d
Adrian Sieber <mail@adriansieber.com>
2017-12-04 12:47:04 +0000

    Add service to serve a personal landing page

---
+++ services/home/functions/__main__.js
@@ -0,0 +1,49 @@
+const path = require('path')
+
+const pug = require('pug')
+const fse = require('fs-extra')
+const yaml = require('js-yaml')
+
+
+async function getHtml () {
+  const compiledTemplate = pug.compileFile(
+    path.resolve(__dirname, '../views/home.pug')
+  )
+  const dataFilePath = path.resolve(__dirname, '../data.yaml')
+  const dataYaml = await fse.readFile(dataFilePath)
+  const data = yaml.safeLoad(dataYaml)
+  const html = compiledTemplate(data.home)
+
+  return html
+}
+
+
+/**
+* Home page
+* @param {String} token Authentication token
+* @returns {Buffer}
+*/
+// eslint-disable-next-line id-blacklist
+module.exports = (token = '', context, callback) => {
+  if (token !== process.env.homeToken) {
+    callback(
+      null,
+      new Buffer('Not Found'),
+      {status: 404},
+    )
+    return
+  }
+
+  // Returning HTML is not yet supported for async functions
+  // therefore the callback variant is used
+  // (and stdlib requries the variable to be called `callback`)
+  getHtml(context)
+    .then(html => {
+      callback(
+        null,
+        Buffer.from(html),
+        {'Content-Type': 'text/html; charset=utf-8'},
+      )
+    })
+    .catch(error => callback(error))
+}
