861fc171a54bc199f7bc950d64e0a1ee131f631d
Adrian Sieber <mail@adriansieber.com>
2018-10-17 21:24:29 +0000

    Add yarn lock file

--- server.js
+++ server.js
@@ -44,8 +44,7 @@ module.exports = app
 
 if (!isMounted) {
   const port = 3000
-  app.set('view engine', 'jade')
+  app.set('view engine', 'pug')
   app.listen(port)
-  // eslint-disable-next-line no-console
-  console.log(`App listens on http://localhost:${port}`)
+  console.info(`App listens on http://localhost:${port}`)
 }
