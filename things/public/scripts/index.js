b31a30328a0f160355790e0ab9a5ce868d72d0c4
Adrian Sieber <mail@adriansieber.com>
2020-01-01 20:48:42 +0000

    Extend categorization of things

--- things/public/scripts/index.js
+++ things/public/scripts/index.js
@@ -2,71 +2,181 @@
 /* global things, shaven */
 
 const typeMap = {
-  jeans: 'pants',
-  chino: 'pants',
-  corduroys: 'pants',
-  trousers: 'pants',
+  // Pants
+  'jeans': 'pants',
+  'chino': 'pants',
+  'corduroys': 'pants',
+  'trousers': 'pants',
 
   // TODO: Reverse map direction and allow injective entries
   // 'pajama pants': 'pants',
   'pajama pants': 'pajamas',
 
+  // Bicycle
   'bicycle-gear': 'bicycle',
+  'stem': 'bicycle',
+  'chain': 'bicycle',
 
-  boxershorts: 'boxers',
+  // Boxers
+  'boxershorts': 'boxers',
   'boxer shorts': 'boxers',
 
-  pocketknife: 'tool',
-  'pocket knife': 'tool',
-  multimeter: 'tool',
-  'digital caliper': 'tool',
-  ruler: 'tool',
-  tweezers: 'tool',
+  // Shorts
+  'boardshorts': 'shorts',
 
-  belt: 'belt',
+  // Tool
+  'craft': 'tool',
+  'digital caliper': 'tool',
+  'glue-gun': 'tool',
+  'multimeter': 'tool',
+  'pocket knife': 'tool',
+  'pocketknife': 'tool',
+  'ruler': 'tool',
+  'solder-station': 'tool',
+  'tweezers': 'tool',
+
+  // Battery
+  'button cell': 'battery',
+
+  'belt': 'belt',
 
   'button up': 'button-up',
   'button-up': 'button-up',
 
-  adapter: 'cable',
-  wire: 'cable',
+  // Cable
+  'adapter': 'cable',
+  'wire': 'cable',
   'hdmi wire': 'cable',
   'hdmi-wire': 'cable',
-  cable: 'cable',
+  'cable': 'cable',
+  'cord': 'cable',
   'power cord': 'cable',
   'power-cord': 'cable',
 
-  coat: 'jacket',
+  // Headgear
+  'beanie': 'headgear',
+  'hat': 'headgear',
+  'scarf': 'headgear',
 
-  pullover: 'sweater',
-  sweatshirt: 'sweater',
+  'coat': 'jacket',
+
+  // Sweater
+  'fleece': 'sweater',
+  'longsleeve': 'sweater',
+  'pullover': 'sweater',
+  'sweatshirt': 'sweater',
   'sweat shirt': 'sweater',
-  hoodie: 'sweater',
+  'hoodie': 'sweater',
   'hooded zipper': 'sweater',
 
   // Music gear
-  microphone: 'music-gear',
-  instrument: 'music-gear',
+  'acoustic adhesive': 'music-gear',
   'alto-saxophone': 'music-gear',
-  saxophone: 'music-gear',
-  headphones: 'music-gear',
-  earphones: 'music-gear',
+  'capo': 'music-gear',
+  'drumhead': 'music-gear',
+  'ear-plugs': 'music-gear',
+  'earphones': 'music-gear',
+  'headphones': 'music-gear',
+  'instrument': 'music-gear',
+  'keyboard': 'music-gear',
+  'microphone': 'music-gear',
+  'midi-controller': 'music-gear',
+  'saxophone': 'music-gear',
+  'sound-absorber': 'music-gear',
+  'tenor-saxophone': 'music-gear',
+  'ukulele': 'music-gear',
 
   // Houseware
-  homeware: 'houseware',
+  'bedframe': 'houseware',
+  'blanket': 'houseware',
+  'furniture': 'houseware',
+  'homeware': 'houseware',
+  'pillow': 'houseware',
+  'sheet': 'houseware',
+  'washing machine': 'houseware',
 
-  stockings: 'socks',
+  // 'Kitchenware'
+  'bottle': 'kitchenware',
+  'cup': 'kitchenware',
+  'dishes': 'kitchenware',
+  'plate': 'kitchenware',
 
+  // Baggage
+  'backpack': 'baggage',
+  'bag': 'baggage',
+  'case': 'baggage',
+  'container': 'baggage',
+  'suitcase': 'baggage',
+  'wallet': 'baggage',
+
+  // Gadget
+  '3d-printer': 'gadget',
+  'calculator': 'gadget',
+  'charger': 'gadget',
+  'display': 'gadget',
+  'hard-drive': 'gadget',
+  'hub': 'gadget',
+  'keyboard': 'gadget',
+  'laptop': 'gadget',
+  'lock': 'gadget',
+  'music-player': 'gadget',
+  'mouse': 'gadget',
+  'micro-sd-card': 'gadget',
+  'monitor': 'gadget',
+  'nas': 'gadget',
+  'padlock': 'gadget',
+  'paracord': 'gadget',
+  'printer': 'gadget',
+  'phone': 'gadget',
+  'printer cartridge': 'gadget',
+  'router': 'gadget',
+  'repeater': 'gadget',
+  'shaver': 'gadget',
+  'sim-card': 'gadget',
+  'smartphone': 'gadget',
+  'storage-card': 'gadget',
+  'stylus': 'gadget',
+  'tablet': 'gadget',
+  'tripod': 'gadget',
+  'usb-stick': 'gadget',
+  'usb hub': 'gadget',
+  'audio system': 'gadget',
+
+  // Sport
+  'racket': 'sport',
+  'frisbee': 'sport',
+  'back-protector': 'sport',
+
+  // DIY
+  'solder': 'diy',
+  'craft-supply': 'diy',
+  'pencil': 'diy',
+
+  // Sanitary Articles
+  'sanitary-product': 'sanitary-article',
+  'hygiene-article': 'sanitary-article',
+  'hygiene-product': 'sanitary-article',
+  'lip-balm': 'sanitary-article',
+  'shampoo': 'sanitary-article',
+  'deodorant': 'sanitary-article',
+  'hair powder': 'sanitary-article',
+  'perfume': 'sanitary-article',
+
+  'stockings': 'socks',
+
+  // T-Shirt
   'polo shirt': 't-shirt',
 
-  boots: 'shoes',
-  'hiking-boots': 'shoes',
-  chucks: 'shoes',
-  'flip-flops': 'shoes',
+  // Shoes
+  'boots': 'shoes',
+  'chucks': 'shoes',
   'flip flops': 'shoes',
-  sneakers: 'shoes',
-  'ski-boots': 'shoes',
+  'flip-flops': 'shoes',
+  'hiking-boots': 'shoes',
+  'running-shoes': 'shoes',
   'ski boots': 'shoes',
+  'ski-boots': 'shoes',
+  'sneakers': 'shoes',
 }
 
 function hideSpinner (element) {
